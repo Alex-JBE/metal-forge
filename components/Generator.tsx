1215dcc3d9c83295ad18703a0eea24ebc3033393
Alex-JBE <alex.von.schulz@gmail.com>
2026-04-26 15:32:32 +0100

    debug: add alert to handleSavePdf

--- components/Generator.tsx
+++ components/Generator.tsx
@@ -183,6 +183,8 @@ export default function Generator({ lang, onResult, onGenreChange, onContentType
   };
 
   const handleSavePdf = async () => {
+    console.log('handleSavePdf called', { lyricsText, musicPromptText });
+    alert('PDF starting...');
     if (!lyricsText && !musicPromptText) return;
     try {
       const titleText = extractSongTitle(lyricsText);
